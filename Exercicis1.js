18b64f587f2c34c57a25de6b7edb78c2fe17537b
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-22 18:09:24 +0100

    Exercici 4

--- Exercicis1.js
+++ Exercicis1.js
@@ -1,9 +1,4 @@
-console.log(variable)
-var variable = 'Prova1'
-console.log(variable)
-
-
-console.log(variable2)
-let variable2 = 'Prova2'
-console.log(variable2)
-
+var posicio = 'primer'
+console.log(posicio)
+var posicio = 'segon'
+console.log(posicio)
