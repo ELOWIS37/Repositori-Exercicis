9348cadda15cec94d9afd14a36d79507f58b3385
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-22 23:02:39 +0100

    Exercici 7

--- Exercicis1.js
+++ Exercicis1.js
@@ -1,31 +1,4 @@
-function myRound(num, dec){
-    var exp = Math.pow(10, dec || 1);
-    return parseInt(num * exp, 10) / exp;
+Math.hypotenusa = Math.hypot || function(x,y){ 
+    return Math.sqrt(x*x + y*y)
 }
-//suma
-function suma(a, b){
-    return a+b;
-}
-let resultat1 = suma(1.234, 5.678);
-console.log('Suma = ', myRound(resultat1, 1));
-
-//resta
-function resta(a, b){
-    return a-b;
-}
-let resultat2 = resta(1.234, 5.678);
-console.log('Resta = ', myRound(resultat2, 1));
-
-//divisió
-function divisio(a, b){
-    return a/b;
-}
-let resultat3 = divisio(1.234, 5.678);
-console.log('Divisió = ', myRound(resultat3, 1));
-
-//multiplicació
-function mult(a, b){
-    return a*b;
-}
-let resultat4 = mult(1.234, 5.678);
-console.log('Multiplicació = ', myRound(resultat4, 1));
+var x = 4, y = 4; document.write(Math.hypot(x, y))
