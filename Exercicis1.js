e9898508cf46c3b440d76bcbd87b1561be027e19
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-22 23:17:07 +0100

    Exercici 9

--- Exercicis1.js
+++ Exercicis1.js
@@ -1,5 +1,2 @@
-function div(a, b){
-    return a/b
-}
-let operacio = div('text', 2)
-console.log('Resultat operacio: ' + operacio)
+let nom = 'Eloy Castaño Molina'
+console.log(nom.substr(5,8) + ' -> El cognom té 7 caràcters') 
