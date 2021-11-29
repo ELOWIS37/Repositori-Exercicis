ec6ee095b7766d823adcad499fc7ebe0fbc9ac10
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-29 23:21:06 +0100

    Exercici 9

--- ExercicisFuncions.js
+++ ExercicisFuncions.js
@@ -1,6 +1,6 @@
-function fsum(Num1, Num2){
+var fsum = function(Num1, Num2){
   if (Num1 + Num2 == 30 || Num1 == 30 || Num2 == 30){
     return true
   }
 }
-console.log('Suma de 15 + 15 = ' + fsum(15, 15))
+console.log('Suma de 15 + 30 = ' + fsum(15, 30))
