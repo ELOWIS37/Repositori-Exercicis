4bfc7452a6af1dedd8490fbf08c85e195e989f84
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-29 23:46:07 +0100

    Exercici 10

--- ExercicisFuncions.js
+++ ExercicisFuncions.js
@@ -1,6 +1,3 @@
-var fsum = function(Num1, Num2){
-  if (Num1 + Num2 == 30 || Num1 == 30 || Num2 == 30){
-    return true
-  }
-}
-console.log('Suma de 15 + 30 = ' + fsum(15, 30))
+const aquadrada = calcul => Math.sqrt(calcul).toFixed(2)
+
+console.log(aquadrada(76))
