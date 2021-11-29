95aeaa241e3d78598a379d9f80d67acd3c875332
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-29 22:41:25 +0100

    Exercici 7

--- ExercicisFuncions.js
+++ ExercicisFuncions.js
@@ -2,8 +2,14 @@ let Sum1 = 2
 let Sum2 = 4
 let Sum3 = 6
 
+function funciodata(hoy){
+  var data = new Date();
+console.log(data.getDate() + '/' + data.getMonth() + '/' + data.getFullYear())
+
 function fsum(){
   return Sum1, Sum2, Sum3;
+}
 
 }
-console.log(Sum1 + Sum2 + Sum3);
+funciodata();
+console.log(Sum1 + Sum2 + Sum3)
