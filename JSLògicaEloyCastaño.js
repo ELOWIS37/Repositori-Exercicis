04033881baeab91875cdc8ee5a0ed47444abb68a
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-11-30 09:40:31 +0100

    Exercici 2

--- JSLògicaEloyCastaño.js
+++ JSLògicaEloyCastaño.js
@@ -1,7 +1,7 @@
-var num = 14
-if (num%3 == 0){
-    console.log('El número ' + num + ' és multiple de 3')
-}
-if (num%7 == 0){
-    console.log('El número ' + num + ' és multiple de 7')
-}
+var x = 50
+var y = 9
+x >= 10 && x <= 50 ?    console.log('El numero ' + x + ' està entre el rang 10 - 50')
+                  :     console.log('El numero ' + x + ' no està entre el rang 10 - 50')   
+
+y >= 10 && y <= 51 ?    console.log('El numero ' + y + ' està entre el rang 10 - 50')
+                   :    console.log('El numero ' + y + ' no està entre el rang 10 - 50')
