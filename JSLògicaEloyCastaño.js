0b81478ef2fc8a4d8213959cd2ef7f6c048b3081
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-12-01 23:33:28 +0100

    Exercici 6

--- JSLògicaEloyCastaño.js
+++ JSLògicaEloyCastaño.js
@@ -1,9 +1,14 @@
-var NumeroFix = 15
-var acumulador = 1
-var suma = 1
-while (suma <= NumeroFix){
-    acumulador + 1
-    console.log(suma + ' + ')
-    var suma = suma + acumulador
+var Num1 = 43
+var Num2 = 22
+
+if (Num1 < Num2) {
+    var menor = Num1
+    var major = Num2
+    console.log('El numero ' + major + ' és menor que el numero ' + menor) 
 }
-console.log(' = ' + NumeroFix)
+
+else if (Num1 > Num2){
+    var major = Num1
+    var menor = Num2
+    console.log('El numero ' + major + ' és mes gran que el numero ' + menor)
+}
