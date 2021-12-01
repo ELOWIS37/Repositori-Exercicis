e61e19a2ae62d8b631ec5c865e76a09a0e86eeef
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-12-01 23:54:41 +0100

    Exercici 9

--- JSLògicaEloyCastaño.js
+++ JSLògicaEloyCastaño.js
@@ -1,5 +1,18 @@
-const asterisc = '*'
+var numeros = 0
+var m3 = 1
+var m5 = 1
+do{
+    if (m3 % 3 == 0){
+    m3 = m3 + numeros
+    console.log(numeros)
+    }
+    numeros++
+}while (numeros <= 1000)
 
-for (var i = 1; i < 5; i++){
-    console.log(asterisc.repeat(i))
-}
+do{
+    if (m5 % 5 == 0){
+    m5 = m5 + numeros
+    console.log(numeros)
+    }
+    numeros++
+}while (numeros <= 1000)
