181c5e4a9c291856c870dc7d8cf4f4e1a68163b6
ELOWIS37 <eloy.castano@institutvidreres.cat>
2021-12-01 23:40:46 +0100

    Exercici 8

--- JSLògicaEloyCastaño.js
+++ JSLògicaEloyCastaño.js
@@ -1,14 +1,5 @@
-var Num1 = 43
-var Num2 = 22
+const asterisc = '*'
 
-if (Num1 < Num2) {
-    var menor = Num1
-    var major = Num2
-    console.log('El numero ' + major + ' és menor que el numero ' + menor) 
-}
-
-else if (Num1 > Num2){
-    var major = Num1
-    var menor = Num2
-    console.log('El numero ' + major + ' és mes gran que el numero ' + menor)
+for (var i = 1; i < 5; i++){
+    console.log(asterisc.repeat(i))
 }
