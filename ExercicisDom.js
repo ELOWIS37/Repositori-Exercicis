a3524a871df7fcc0241dc234390c2510145e98ee
ELOWIS37 <eloy.castano@institutvidreres.cat>
2022-03-03 20:30:14 +0100

    Exercici 6

--- ExercicisDom.js
+++ ExercicisDom.js
@@ -1,8 +1,4 @@
-function cambiarLletra() {
-    var llista1 = document.getElementsByClassName("lista")
-    var llista2 = llista1[0].children
-    for (let i = 0; i < llista2.length; i++){
-    var text = llista2[i].innerHTML.slice(1);
-    llista2[i].innerHTML = llista2[i].innerHTML.charAt(0).fontsize(25) + text;
-    }
+export function contarParaules(text) {
+    var Paraules = text.split(" ");
+    return Paraules.length;
 }
