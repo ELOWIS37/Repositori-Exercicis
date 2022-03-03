ad6cfdb74b2ab4ea8ec3827b0b9b845df8376569
ELOWIS37 <eloy.castano@institutvidreres.cat>
2022-03-03 20:55:08 +0100

    Exercici 7

--- ExercicisDom.js
+++ ExercicisDom.js
@@ -1,4 +1,4 @@
-export function contarParaules(text) {
-    var Paraules = text.split(" ");
-    return Paraules.length;
-}
+function mostrarImatge() {
+    document.body.style.backgroundImage = "url('https://cdn.thinglink.me/api/image/865265753459261440/1024/10/scaletowidth/0/0/1/1/false/true?wait=true')";
+  }
+
