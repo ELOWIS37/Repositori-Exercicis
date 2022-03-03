223d223ce81cf184e7620deb8c8ccdd35b3c9e77
ELOWIS37 <eloy.castano@institutvidreres.cat>
2022-03-03 09:54:15 +0100

    Exercici 5

--- ExercicisDom.js
+++ ExercicisDom.js
@@ -1,4 +1,8 @@
-function cambiarText(x, y){
-document.getElementById("Taula").width = x; 
-document.getElementById("Taula").height = y;
+function cambiarLletra() {
+    var llista1 = document.getElementsByClassName("lista")
+    var llista2 = llista1[0].children
+    for (let i = 0; i < llista2.length; i++){
+    var text = llista2[i].innerHTML.slice(1);
+    llista2[i].innerHTML = llista2[i].innerHTML.charAt(0).fontsize(25) + text;
+    }
 }
