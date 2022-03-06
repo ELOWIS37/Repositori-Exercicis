ed537798577dc2f6daf4b91668e9f0c13adec879
ELOWIS37 <eloy.castano@institutvidreres.cat>
2022-03-07 00:43:20 +0100

    Exercici 6

--- ExercicisEstructures.js
+++ ExercicisEstructures.js
@@ -1,7 +1,17 @@
-var numeros = [ 1, 3, 6 ];
-
-Object.keys(numeros).map(function(clau) {
-   numeros[clau] = Math.pow(numeros[clau],3);
-});
-
-console.log(numeros);
+var paraules = [" aixecar ", " estudiar ", " pc ", " pilota ", " administratiu ", " aclarir "]
+var arrayDeParaules = [];
+var acomulador = 0
+var contador = 0
+for(var i = 0; i < paraules.length; i++) {
+  for(var ii = 0; ii < paraules[i].length; ii++){
+	  if (paraules[i][ii] == 'a'){
+      contador++ 
+      if (contador == 2){
+        arrayDeParaules[acomulador] = paraules[i];
+        acomulador++
+      }
+    }
+  }
+  contador = 0
+}
+document.write("Paraules amb més d'una lletra 'acomulador' -> " + arrayDeParaules)
