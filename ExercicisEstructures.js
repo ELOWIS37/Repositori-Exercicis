c405bbdd41dc67c974cd6b899aff32367453abd0
ELOWIS37 <eloy.castano@institutvidreres.cat>
2022-03-07 00:08:23 +0100

    Exercici 3

--- ExercicisEstructures.js
+++ ExercicisEstructures.js
@@ -1,4 +1,3 @@
-var numero = Array.from({length: 5}, () => Math.floor(Math.random() * 10));
-var arrayPropagacio = [10, ...numero, 20];
-document.write("Numeros ordenats de menor a major: ")
-document.write(arrayPropagacio.sort());
+var numeros = [0,1,2,3,4,4];
+var arraySet = new Set([5,6,7,...numeros,8,9,9]);
+console.log(arraySet);
